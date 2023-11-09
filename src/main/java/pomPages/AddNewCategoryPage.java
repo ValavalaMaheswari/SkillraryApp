13ef8ad1abb16cc392a1e32eced88025c6c38a14
ValavalaMaheswari <149999464+ValavalaMaheswari@users.noreply.github.com>
2023-11-09 11:21:55 +0530

    Update AddNewCategoryPage.java

--- src/main/java/pomPages/AddNewCategoryPage.java
+++ src/main/java/pomPages/AddNewCategoryPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+// The test is sucess
 
 public class AddNewCategoryPage {
 	//Declaration
